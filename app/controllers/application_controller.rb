88dfb7f6787bd6050a7d1a8093db57e610be029c
LevitatingBusinessMan <public@reinfernhout.xyz>
2022-07-24 21:05:20 +0200

    hide admin buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+	helper_method :logged_in?
+
 	def require_login
 		redirect_to controller: "home", action: "login" unless session[:user]
+	end
+
+	def logged_in?
+		session.key? :user
 	end
 end
