9f350a9670576305c1e0541e1c335b92275b2cda
LevitatingBusinessMan <public@reinfernhout.xyz>
2023-01-11 00:14:26 +0100

    fixed the view somewhat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,19 @@
 require 'redcarpet'
 
 module ApplicationHelper
-	@@redcarpet = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+	@@redcarpet = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+		fenced_code_blocks: true,
+		quote: true,
+		footnotes: true,
+		lax_spacing: true,
+		superscript: true,
+		highlight: true,
+		strikethrough: true,
+		underline: true,
+		autolink: true,
+		disable_indented_code_blocks: true,
+		no_intra_emphasis: true,
+	)
 	def markdown(source)
 		@@redcarpet.render(source)
 	end
