0c1c31242b1e60cacb7750f6843444d1d1280bb1
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2023-06-23 09:54:40 +0200

    dynamic title

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,10 @@ class Article < ApplicationRecord
 	validates :title, presence: true
 	validates :body, presence: true
 	validates :visible, presence: true, allow_blank: true
+
+	# Strip all tags from the title
+	def title_stripped
+		title.gsub(/<\w+>/,"").strip
+	end
+
 end
